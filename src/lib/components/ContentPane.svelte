53fb51a3a4b8154f1739f4e00d8f575dcd52fe0e
Brandon Chang <brandon@brandonchang.me>
2022-12-10 14:16:28 -0800

    un lfs

--- src/lib/components/ContentPane.svelte
+++ src/lib/components/ContentPane.svelte
@@ -1,3 +1,50 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2ae99b0edc2d4696b2e697b26febe9955f68d222f85ece9c829caa42d0341ab9
-size 1137
+<script lang="ts">
+	import Line from '$lib/components/Line.svelte';
+	import type { MediaFile } from '$lib/models/mediaFile';
+	import { active, transcripts } from '$lib/stores/transcripts';
+	import Button from './Button.svelte';
+
+	function handleTranscribe(file: MediaFile) {
+		transcripts.startTranscription(file);
+	}
+</script>
+
+<div class="content-pane">
+	{#if $active}
+		{@const a = $active}
+		{#if a.status !== 'transcribed'}
+			<div class="transcribe-ready">
+				File ready to be transcribed
+				<Button on:click={() => handleTranscribe(a.file)} disabled={a.status === 'transcribing'}>
+					{#if a.status === 'transcribing'}
+						Transcribing...
+					{:else}
+						Start Transcribing
+					{/if}
+				</Button>
+			</div>
+		{/if}
+		{#each $active.rawOutput as line}
+			<Line data={line} />
+		{/each}
+	{:else}
+		<div class="transcribe-ready">Select a transcript</div>
+	{/if}
+</div>
+
+<style>
+	.content-pane {
+		background: var(--neutral-200);
+		grid-area: main;
+		display: flex;
+		flex-direction: column;
+		padding: 12px 0;
+		overflow-y: scroll;
+	}
+
+	.transcribe-ready {
+		text-align: center;
+		width: 300px;
+		margin: auto;
+	}
+</style>
